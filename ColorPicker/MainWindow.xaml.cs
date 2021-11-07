71bd4e8b68da336eb3e15c529d1adfd2e9c2413c
Jerzy <jurekbalcer@gmail.com>
2021-11-07 19:30:36 +0100

    basic ARGB picker

--- ColorPicker/MainWindow.xaml.cs
+++ ColorPicker/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace ColorPicker
 {
@@ -20,9 +8,17 @@ namespace ColorPicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private PickerViewModel _viewModel;
         public MainWindow()
         {
             InitializeComponent();
+            _viewModel = new PickerViewModel();
+            DataContext = _viewModel;
+        }
+
+        private void ColorSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            _viewModel.PickNewColor();
         }
     }
 }
---
+++ ColorPicker/PickerViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+
+namespace ColorPicker
+{
+    public class PickerViewModel : INotifyPropertyChanged
+    {
+        public SolidColorBrush PickedColor { get; set; } = new SolidColorBrush(Color.FromArgb(255,0,0,0));
+
+        private string _pickedColorHex;
+        public string PickedColorHex
+        {
+            get { 
+                return "#"+AlphaValue.ToString("X2") + RedValue.ToString("X2") + GreenValue.ToString("X2") + BlueValue.ToString("X2"); 
+            }
+            set { _pickedColorHex = value; }
+        }
+
+
+        public int RedValue { get; set; } = 0;
+        public int GreenValue { get; set; } = 0;
+        public int BlueValue { get; set; } = 0;
+        public int AlphaValue { get; set; } = 255;
+
+        public void PickNewColor()
+        {
+            PickedColor = new SolidColorBrush(Color.FromArgb((byte)AlphaValue, (byte)RedValue, (byte)GreenValue, (byte)BlueValue));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
